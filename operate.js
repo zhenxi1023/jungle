895d39b7ea8b83b4f2dfbfc46b85a12862a25901
Zhenxi Zhang <zhenxi1023@gmail.com>
2020-07-02 22:26:05 +0800

    fix

--- operate.js
+++ operate.js
@@ -1,5 +1,19 @@
-let buttons = document.getElementById("ayb-reviews").querySelectorAll("button");
-if (buttons && buttons > 1) {
-  let yes = buttons[1].click();
-  window.close();
+
+document.onreadystatechange = function () {
+  if (document.readyState === 'complete') {
+    operate();
+  }
+};
+
+
+
+function operate() {
+
+  let buttons = document.getElementById("ayb-reviews").querySelectorAll("button");
+  if (buttons && buttons > 1) {
+    let yes = buttons[1].click();
+    window.close();
+  }
+
 }
+
